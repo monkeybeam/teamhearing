ce8b91357b6405d335969b0ac9accdfe916aa2c3
Dennis Bacsafra <dbacsafra@gmail.com>
2012-11-14 15:06:24 -0800

    adding riffwave

--- www/js/audio.js
+++ www/js/audio.js
@@ -158,3 +158,12 @@ function play_video(url) {
 						+"</video>";
 		document.getElementById("videopanel").innerHTML=videocontent;
 }
+
+//RIFFWAVE
+function play_riffwave() {
+	var data = []; // just an array
+	for (var i=0; i<10000; i++) data[i] = Math.round(255 * Math.random()); // fill data with random samples
+	var wave = new RIFFWAVE(data); // create the wave file
+	var audio = new Audio(wave.dataURI); // create the HTML5 audio element
+	audio.play(); // some noise
+}
