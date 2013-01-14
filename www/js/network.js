c66780061236e215cb6d7d5fcb1c89bb6db3a859
Dennis Bacsafra <dbacsafra@gmail.com>
2013-01-14 13:28:01 -0800

    removed hrilogin.js
    replaced with MobileLogin in network.js

--- www/js/network.js
+++ www/js/network.js
@@ -166,6 +166,31 @@ function onResume() {
 	} 
 }
 
+function MobileLogin()
+{
+	//get variables
+	var loginusername=document.getElementById("username").value;
+	var loginpassword=document.getElementById("password").value;
+	var loginversion=document.getElementById("version").value;
+	//remove all the class add the messagebox classes and start fading
+	//document.getElementById("msgbox").removeClass().addClass('messagebox').text('Validating....').fadeIn(1000);
+	document.getElementById("msgbox").innerHTML="Validating...";
+	//check the username exists or not from ajax
+	$.post("https://www.teamaudiology.org/phonegap/php/dbcontrol.php",{ action:'login',username:loginusername,password:loginpassword,version:loginversion,rand:Math.random() } ,function(data)
+	{
+	  if(data=='no')  //login failed
+	  {
+		document.getElementById("msgbox").innerHTML="Your login detail failed..";
+	  }
+	  else //the login detail is correct
+	  {
+		document.location='#page3';
+	  }
+	});
+	
+	return false; //not to post the  form physically
+}
+
 function showDatabaseStats() {
 	document.getElementById("checkdatabase").innerHTML="Database is Connected";
 	document.getElementById("checkdatabasemembers").innerHTML=jsonmemberscount;
