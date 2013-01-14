dc5875a1c810fcea0404053af0cde31512cabad8
Dennis Bacsafra <dbacsafra@gmail.com>
2013-01-14 11:47:56 -0800

    same

--- www/js/hrilogin.js
+++ www/js/hrilogin.js
@@ -7,7 +7,8 @@ function StartLogin()
 	var loginversion=document.getElementById("version").value;
 	alert(loginusername);
 	//remove all the class add the messagebox classes and start fading
-	document.getElementById("msgbox").removeClass().addClass('messagebox').text('Validating....').fadeIn(1000);
+	//document.getElementById("msgbox").removeClass().addClass('messagebox').text('Validating....').fadeIn(1000);
+	document.getElementById("msgbox").innerHTML="Validating...";
 	alert(loginpassword);
 	//check the username exists or not from ajax
 	$.post("https://www.teamaudiology.org/phonegap/php/dbcontrol.php",{ action:'login',username:loginusername,password:loginpassword,version:loginversion,rand:Math.random() } ,function(data)
@@ -15,45 +16,30 @@ function StartLogin()
 	  if(data=='no')  //login failed
 	  {
 		alert("login failed");
-		document.getElementById("msgbox").fadeTo(200,0.1,function() //start fading the messagebox
-		{ 
-		  //add message and change the class of the box and start fading
-		  document.getElementById("msgbox").html('Your login detail failed...').addClass('messageboxerror').fadeTo(900,1);
-		});		
+		document.getElementById("msgbox").innerHTML="Your login detail failed..";
 	  }
 	  else //the login detail is correct
 	  {
 		alert("login worked");
-		document.getElementById("msgbox").fadeTo(200,0.1,function()  //start fading the messagebox
-		{ 
-		  //add message and change the class of the box and start fading
-		  document.getElementById("msgbox").html('Logging in.....').addClass('messageboxok').fadeTo(900,1,
-		 function()
-		  { 
-			//redirect to secure homepage
-			alert(loginversion);
-			switch (loginversion)
-			{
-			case "professional":
-				document.location='pro.php';
-				break;
-			case "legacy":			
-				document.location='legacyhome.php';
-				break;
-			case "shell":
-				document.location='shellonly.php';
-				break;
-			case "mobile":
-				alert("at mobile");
-				document.location='index.html#page3';				
-				break;
-			default:
-				document.location='pro.php';
-			}
-		  });			  
-		});
-	  }
-			
+		document.getElementById("msgbox").innerHTML="Logging in..";
+		switch (loginversion)
+		{
+		case "professional":
+			document.location='pro.php';
+			break;
+		case "legacy":			
+			document.location='legacyhome.php';
+			break;
+		case "shell":
+			document.location='shellonly.php';
+			break;
+		case "mobile":
+			alert("at mobile");
+			document.location='index.html#page3';				
+			break;
+		default:
+			document.location='pro.php';
+		}		
 	});
 	
 	return false; //not to post the  form physically
